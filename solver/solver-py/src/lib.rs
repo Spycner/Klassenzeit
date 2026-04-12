8562ff5786a3e0ffd12061794d1b2e845a180bd2
pgoell <pascal98kraus@gmail.com>
2026-04-12 13:47:11 +0200

    refactor: rename PyO3 wrapper fn to py_reverse_chars for global uniqueness

--- solver/solver-py/src/lib.rs
+++ solver/solver-py/src/lib.rs
@@ -6,13 +6,14 @@ use pyo3::prelude::*;
 
 /// Reverse the characters in a string (PyO3 wrapper).
 #[pyfunction]
-fn reverse_chars(s: &str) -> String {
+#[pyo3(name = "reverse_chars")]
+fn py_reverse_chars(s: &str) -> String {
     solver_core::reverse_chars(s)
 }
 
 /// Python module exposing solver-core functions.
 #[pymodule]
 fn _rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(reverse_chars, m)?)?;
+    m.add_function(wrap_pyfunction!(py_reverse_chars, m)?)?;
     Ok(())
 }
